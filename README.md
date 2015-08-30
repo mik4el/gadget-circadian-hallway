2aec9b171b86d6bdb4fb880012b05240d795ef3a
Mikael Andersson <mik4el.4ndersson@gmail.com>
2015-08-30 17:51:52 +0200

    small updates to cycle colors

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # circadian_hallway
+Control color and intensity of led strips in a hallway to match optimal human circadian rhythm. Arduino and python interface to custom electronics for 200W RGB leds.
+
+# Suggested hardware
+Raspi, Arduino Pro Micro (but it is a hazzle to program from commandline), RGB leds with common anode and transistors to control from Arduino.
+
+# Todos
+1. Take serial commands from python with color and animation time, animate to that color in the given time.
+1. Python command that sets color for circadian rythm depending on time of day.
--- circadian_hallway_cycle_colors/circadian_hallway_cycle_colors.ino
+++ circadian_hallway_cycle_colors/circadian_hallway_cycle_colors.ino
@@ -9,8 +9,8 @@ byte BLUE_A = 0;
 int led_delay = 0;
 byte color_count = 1;                //Count the colors out
 #define color_count_max 5              //Set this to the max number of colors defined
-#define color_delay 20000             //Define the delay between changing colors in ms
-#define time_at_color 1           //Time to stay on a color in ms
+#define color_delay 60000             //Define the delay between changing colors in ms
+#define time_at_color 0           //Time to stay on a color in ms
 
 //Some Time values
 unsigned long TIME_LED = 0;
@@ -37,6 +37,8 @@ unsigned long TIME_color = 0;
 #define C5_G 10
 #define C5_B 0
 
+#define MAX_COLOR_DIFFERENCE 55
+
 void setup()
 {
 
@@ -45,8 +47,9 @@ void setup()
   GREEN = C1_G;
   BLUE = C1_B;
   //Get the led_delay speed
-  led_delay = (color_delay - time_at_color) / 255; 
-
+  led_delay = (color_delay - time_at_color) / MAX_COLOR_DIFFERENCE; 
+  if (led_delay > 41) led_delay = 41;
+  
   analogWrite(GRN_PIN, 0);
   analogWrite(RED_PIN, 0);
   analogWrite(BLU_PIN, 0);
@@ -58,14 +61,14 @@ void loop()
 
   //Rest of your program - Avoid using delay(); function!
 
-  if(millis() - TIME_LED >= led_delay){
+  if (millis() - TIME_LED >= led_delay) {
     TIME_LED = millis();
 
     //Run the LED Function to check and adjust the values
     LED();
   }
 
-  if(millis() - TIME_color >= color_delay){
+  if (millis() - TIME_color >= color_delay) {
     TIME_color = millis();
 
     //Run the color Change function
@@ -78,17 +81,17 @@ void LED()
 {
 
   //Check Values and adjust "Active" Value
-  if(RED != RED_A){
-    if(RED_A > RED) RED_A = RED_A - 1;
-    if(RED_A < RED) RED_A++;
+  if (RED != RED_A) {
+    if (RED_A > RED) RED_A = RED_A - 1;
+    if (RED_A < RED) RED_A++;
   }
-  if(GREEN != GREEN_A){
-    if(GREEN_A > GREEN) GREEN_A = GREEN_A - 1;
-    if(GREEN_A < GREEN) GREEN_A++;
+  if (GREEN != GREEN_A) {
+    if (GREEN_A > GREEN) GREEN_A = GREEN_A - 1;
+    if (GREEN_A < GREEN) GREEN_A++;
   }
-  if(BLUE != BLUE_A){
-    if(BLUE_A > BLUE) BLUE_A = BLUE_A - 1;
-    if(BLUE_A < BLUE) BLUE_A++;
+  if (BLUE != BLUE_A) {
+    if (BLUE_A > BLUE) BLUE_A = BLUE_A - 1;
+    if (BLUE_A < BLUE) BLUE_A++;
   }
 
   //Assign modified values to the pwm outputs for each color led
@@ -102,26 +105,29 @@ void color()
 {
 
   //Increment the color by one or go back to 1 if maxed out
-  if(color_count < color_count_max) color_count++;
-  else color_count = 1;
+  if (color_count < color_count_max) {
+    color_count++;
+  } else {
+    color_count = 1;
+  }
 
-  if(color_count == 1){
+  if (color_count == 1) {
     RED = C1_R;
     GREEN = C1_G;
     BLUE = C1_B;
-  } else if(color_count == 2){
+  } else if (color_count == 2) {
     RED = C2_R;
     GREEN = C2_G;
     BLUE = C2_B;
-  } else if(color_count == 3){
+  } else if (color_count == 3) {
     RED = C3_R;
     GREEN = C3_G;
     BLUE = C3_B;
-  } else if(color_count == 4){
+  } else if (color_count == 4) {
     RED = C4_R;
     GREEN = C4_G;
     BLUE = C4_B;
-  } else if(color_count == 5){
+  } else if (color_count == 5) {
     RED = C5_R;
     GREEN = C5_G;
     BLUE = C5_B;
